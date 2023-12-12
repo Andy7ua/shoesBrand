fe23c294c93b0ddbe25b8f32ad5f69a4fb400197
Andy7ua <129150304+Andy7ua@users.noreply.github.com>
2023-12-12 18:09:01 +0200

    Update img_resizer.py

--- img_resizer.py
+++ img_resizer.py
@@ -1,29 +1,57 @@
-from PIL import Image
 import os
+from PIL import Image
 
-# The path to the folder with the source images
-source_root = 'images/'
 
-# The path to the folder where the resized images will be saved
-output_root = 'resized_img/'
+def resize_image(image_path, output_path, size=(128, 128)):  # desired img resize parameters
+    # Open and resize the image
+    image = Image.open(image_path)
+    resized_image = image.resize(size)
+    resized_image.save(output_path)
 
-new_size = (104, 104)  # set new img size
 
-# Iterate through all subdirectories in the source folder
-for root, dirs, files in os.walk(source_root):
-    for dir in dirs:
-        source_folder = os.path.join(root, dir)
-        output_folder = os.path.join(output_root, dir)
+def process_folders(source_folder):
+    # Create the "dataset" folder
+    dataset_folder = os.path.join(os.getcwd(), 'dataset')
+    os.makedirs(dataset_folder, exist_ok=True)
 
-        # Ensure the output folder exists, create it if it doesn't
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+    # Get the list of folders in the specified directory
+    folders = [f for f in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, f))]
 
-        # Process images in the current source folder
-        for filename in os.listdir(source_folder):
-            if filename.endswith('.jpg') or filename.endswith('.png'):
-                img = Image.open(os.path.join(source_folder, filename))
-                img = img.resize(new_size, Image.BILINEAR)
-                img.save(os.path.join(output_folder, filename))
+    # Find the folder with the smallest number of images
+    min_images_count = float('inf')
+    min_images_folder = None
 
-print("The images from source_folder were successfully resized and saved to output_folder!")
+    for folder in folders:
+        folder_path = os.path.join(source_folder, folder)
+        images_count = len([f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))])
+
+        if images_count < min_images_count:
+            min_images_count = images_count
+            min_images_folder = folder
+
+    print(f"Copying and resizing images. Total folders: {len(folders)}, Minimum images count: {min_images_count}")
+
+    # Move and resize images from the folder with the minimum count
+    for folder in folders:
+        source_path = os.path.join(source_folder, folder)
+        destination_path = os.path.join(dataset_folder, folder)
+
+        # Create a folder in the "dataset" directory
+        os.makedirs(destination_path, exist_ok=True)
+
+        # Process images
+        images_to_process = os.listdir(source_path)[:min_images_count]
+        for i, image_name in enumerate(images_to_process):
+            image_path = os.path.join(source_path, image_name)
+            output_path = os.path.join(destination_path, image_name)
+
+            # Resize the image
+            resize_image(image_path, output_path)
+
+            # Display progress
+            print(f"Processed image {i + 1}/{min_images_count} in folder {folder}")
+
+
+if __name__ == "__main__":
+    folder_path = "/Users/andy/Downloads/images"  # path to your images
+    process_folders(folder_path)
