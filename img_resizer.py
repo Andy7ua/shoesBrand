8ab32a4c188b54432bc0723a2440048401412ac4
Andy7ua <129150304+Andy7ua@users.noreply.github.com>
2024-01-26 01:08:40 +0200

    bugs fixed

--- img_resizer.py
+++ img_resizer.py
@@ -2,22 +2,18 @@ import os
 from PIL import Image
 
 
-def resize_image(image_path, output_path, size=(128, 128)):  # desired img resize parameters
-    # Open and resize the image
+def resize_image(image_path, output_path, size=(128, 128)):
     image = Image.open(image_path)
     resized_image = image.resize(size)
     resized_image.save(output_path)
 
 
 def process_folders(source_folder):
-    # Create the "dataset" folder
     dataset_folder = os.path.join(os.getcwd(), 'dataset')
     os.makedirs(dataset_folder, exist_ok=True)
 
-    # Get the list of folders in the specified directory
     folders = [f for f in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, f))]
 
-    # Find the folder with the smallest number of images
     min_images_count = float('inf')
     min_images_folder = None
 
@@ -31,27 +27,28 @@ def process_folders(source_folder):
 
     print(f"Copying and resizing images. Total folders: {len(folders)}, Minimum images count: {min_images_count}")
 
-    # Move and resize images from the folder with the minimum count
     for folder in folders:
         source_path = os.path.join(source_folder, folder)
         destination_path = os.path.join(dataset_folder, folder)
-
-        # Create a folder in the "dataset" directory
         os.makedirs(destination_path, exist_ok=True)
 
-        # Process images
         images_to_process = os.listdir(source_path)[:min_images_count]
         for i, image_name in enumerate(images_to_process):
             image_path = os.path.join(source_path, image_name)
             output_path = os.path.join(destination_path, image_name)
 
-            # Resize the image
             resize_image(image_path, output_path)
-
-            # Display progress
             print(f"Processed image {i + 1}/{min_images_count} in folder {folder}")
+
+        # Remove excess files in folders with more images
+        if folder != min_images_folder:
+            excess_files = os.listdir(source_path)[min_images_count:]
+            for excess_file in excess_files:
+                excess_file_path = os.path.join(source_path, excess_file)
+                os.remove(excess_file_path)
+                print(f"Removed excess file: {excess_file} in folder {folder}")
 
 
 if __name__ == "__main__":
-    folder_path = "dataset"  # path to your images
+    folder_path = "dataset"
     process_folders(folder_path)
